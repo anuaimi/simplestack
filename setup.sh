a6a929ac46e1da3f3b2f3f8edca0aa829b3ff39e
Athir Nuaimi <athir@nuaimi.com>
2014-05-24 22:35:44 -0400

    use built-in ruby & libvirt

--- setup.sh
+++ setup.sh
@@ -1,37 +1,41 @@
 # sets needed to setup server
 
 # insert rvm
-sudo apt-get install curl
-\curl -sSL https://get.rvm.io | bash
-source ~/.rvm/scripts/rvm
+#sudo apt-get install curl
+#\curl -sSL https://get.rvm.io | bash
+#source ~/.rvm/scripts/rvm
 
 # install ruby
-rvm requirements
-rvm install 2.1
+#rvm requirements
+#rvm install 2.1
 
 # check if HW supports virtualization
-sudo apt-get install cpu-checker
+sudo apt-get install -y cpu-checker
 kvm-ok
 uname -m
 
 # install kvm/libvirt
-sudo apt-get install qemu-kvm libvirt-bin bridge-utils
-sudo apt-get install libvirt-dev
+sudo apt-get install -y qemu-kvm libvirt-bin bridge-utils
+sudo apt-get install -y libvirt-dev
 #sudo apt-get install ubuntu-vm-builder
 sudo adduser `id -un` libvirtd
 
+sudo apt-get install -y ruby-ffi
+sudo apt-get install -y ruby-libvirt
+sudo apt-get install -y cloud-utils
+sudo apt-get install -y libxml2-dev libxslt-dev
+
 # install openvswitch
-sudo apt-get install openvswitch-switch
-sudo ovs-vsctl -- --may-exist add-br br0
-sudo ovs-vsctl -- --may-exist add-port br0 eth0
+#sudo apt-get install -y openvswitch-switch
+#sudo ovs-vsctl -- --may-exist add-br br0
+#sudo ovs-vsctl -- --may-exist add-port br0 eth0
 
 # download ubuntu image
 wget https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
 sudo mv trusty-server-cloudimg-amd64-disk1.img /var/lib/libvirt/images/trusty-server-cloudimg-amd64-disk1.img.dist
 sudo qemu-img convert -O qcow2 /var/lib/libvirt/images/trusty-server-cloudimg-amd64-disk1.img.dist /var/lib/libvirt/images/trusty-server-cloudimg-amd64-disk1.img
-sudo qemu-img resize /var/lib/libvirt/images/trusty-server-cloudimg-amd64-disk1.img +8G 
-sudo apt-get install cloud-utils
+#sudo qemu-img resize /var/lib/libvirt/images/trusty-server-cloudimg-amd64-disk1.img +8G 
 
 # 
-sudo apt-get install libxml2-dev libxslt-dev
+sudo gem install bundler
 bundle install
